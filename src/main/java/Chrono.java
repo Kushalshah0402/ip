eb89e82cf8dec8d63263135b9d1d35deb1657fe1
Kushalshah0402 <kshah2407@gmail.com>
2026-01-24 23:02:32 +0800

    Can add to list and display tasks

--- src/main/java/Chrono.java
+++ src/main/java/Chrono.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Chrono {
 
+    private static int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -18,8 +22,23 @@ public class Chrono {
                 System.out.println("------------------------------------------------------------");
                 break;
             }
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("------------------------------------------------------------");
+                if (taskCount == 0) {
+                    System.out.println(" You have no tasks :).");
+                } else {
+                    System.out.println("Here are your tasks:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("------------------------------------------------------------");
+                continue;
+            }
+            tasks[taskCount] = input;
+            taskCount++;
             System.out.println("------------------------------------------------------------");
-            System.out.println(" " + input);
+            System.out.println(" added: " + input);
             System.out.println("------------------------------------------------------------");
         }
 
