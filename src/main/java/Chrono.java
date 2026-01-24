5e0a234db2bb8cf7392f562733ce32e5aed1fce5
Kushalshah0402 <kshah2407@gmail.com>
2026-01-24 22:47:03 +0800

    Echoes instruction and exits with command bye

--- src/main/java/Chrono.java
+++ src/main/java/Chrono.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Chrono {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("------------------------------------------------------------");
         System.out.println(" Hello! I'm Chrono");
         System.out.println(" What can I do for you?");
         System.out.println("------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------------------------------");
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("------------------------------------------------------------");
+                System.out.println("Shutting down Chrono systems. Goodbye!");
+                System.out.println("------------------------------------------------------------");
+                break;
+            }
+            System.out.println("------------------------------------------------------------");
+            System.out.println(" " + input);
+            System.out.println("------------------------------------------------------------");
+        }
+
+        scanner.close();
     }
 }
