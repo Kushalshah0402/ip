115ee268bf1aea6c2195d0233e9887325659403f
Kushalshah0402 <kshah2407@gmail.com>
2026-01-25 09:41:46 +0800

    Add Task class and support mark/unmark task status

--- src/main/java/Chrono.java
+++ src/main/java/Chrono.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Chrono {
 
     private static int MAX_TASKS = 100;
-    private static String[] tasks = new String[MAX_TASKS];
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -35,7 +35,31 @@ public class Chrono {
                 System.out.println("------------------------------------------------------------");
                 continue;
             }
-            tasks[taskCount] = input;
+            if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println(" Nice! I have marked this task as done:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("------------------------------------------------------------");
+                }
+                continue;
+            }
+            if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsNotDone();
+                    System.out.println("------------------------------------------------------------");
+                    System.out.println(" Oops sorry you still have not done this task:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("------------------------------------------------------------");
+                }
+                continue;
+            }
+            tasks[taskCount] = new Task(input);
             taskCount++;
             System.out.println("------------------------------------------------------------");
             System.out.println(" added: " + input);
